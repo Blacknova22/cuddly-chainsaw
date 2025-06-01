c55ab767d06700a2b7bb39ab1a29589a10911793
Blacknova22 <144703495+Blacknova22@users.noreply.github.com>
2025-06-01 15:33:02 +0200

    app.py

--- LXTRACK/app.py
+++ LXTRACK/app.py
@@ -49,4 +49,5 @@ def phone_form():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+      port = int(os.environ.get('PORT', 8080))  # récupère le port de l'env, sinon 5000 par défaut
+    app.run(host='0.0.0.0', port=port)
